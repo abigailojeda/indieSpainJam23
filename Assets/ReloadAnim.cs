f8831bbb0868a1bbba7e9c8fa8d2dd23441e7a7c
noah cuadrado <119018346+noahcuadrado@users.noreply.github.com>
2023-09-19 14:15:40 +0200

    Update ReloadAnim.cs

--- Assets/ReloadAnim.cs
+++ Assets/ReloadAnim.cs
@@ -5,22 +5,11 @@ using DG.Tweening;
 public class ReloadAnim : MonoBehaviour
 {
     [SerializeField] GameObject GunModel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ReloadAnimation() {
         Sequence Reload = DOTween.Sequence();
-        Reload.Append(GunModel.transform.DOLocalMoveY(0.95f,0.1f,false))
-        .Append(GunModel.transform.DOLocalRotate(new Vector3(360*2,0,0),1f,RotateMode.LocalAxisAdd))
-        .Append(GunModel.transform.DOLocalMoveY(0.875f,0.1f,false));
+        Reload.Append(GunModel.transform.DOLocalMoveY(0.95f,0.15f,false).SetEase(Ease.InBack))
+        .Append(GunModel.transform.DOLocalRotate(new Vector3(-360*6,0,0), 2f,RotateMode.LocalAxisAdd).SetEase(Ease.InOutBack))
+        .Append(GunModel.transform.DOLocalMoveY(0.875f,0.15f,false).SetEase(Ease.OutBack));
     }
 }
