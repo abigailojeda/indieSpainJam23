9f65580814385081c0ee94da0141aa84ab3a95cf
noah cuadrado <119018346+noahcuadrado@users.noreply.github.com>
2023-09-19 15:55:54 +0200

    Reload NOT working

--- Assets/ReloadAnim.cs
+++ Assets/ReloadAnim.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
+
 public class ReloadAnim : MonoBehaviour
 {
-    [SerializeField] GameObject GunModel;
+    [SerializeField]
+    GameObject GunModel;
+   
 
-    public void ReloadAnimation() {
+    public void ReloadAnimation()
+    {
         Sequence Reload = DOTween.Sequence();
-        Reload.Append(GunModel.transform.DOLocalMoveY(0.95f,0.15f,false).SetEase(Ease.InBack))
-        .Append(GunModel.transform.DOLocalRotate(new Vector3(-360*6,0,0), 2f,RotateMode.LocalAxisAdd).SetEase(Ease.InOutBack))
-        .Append(GunModel.transform.DOLocalMoveY(0.875f,0.15f,false).SetEase(Ease.OutBack));
+        if (Reload.IsPlaying() == false)
+        {
+            Reload
+                .Append(GunModel.transform.DOLocalMoveY(-0.065f, 0.15f, false).SetEase(Ease.InBack))
+                .Append(
+                    GunModel.transform
+                        .DOLocalRotate(new Vector3(-360 * 3, 0, 0), 2f, RotateMode.LocalAxisAdd)
+                        .SetEase(Ease.InOutBack)
+                )
+                .Append(
+                    GunModel.transform.DOLocalMoveY(-0.125f, 0.15f, false).SetEase(Ease.OutBack)
+                );
+        }
+        else
+        {
+            return;
+        }
     }
 }
